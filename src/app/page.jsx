c9c5c718aef1c88b739a04f99a00ae7c08bd372b
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-06-19 21:24:37 -0300

    Aula 19-06

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="container"> 
-      <h1>Helo World</h1>
+      <h1 className={styles.titulo}>Helo World</h1>
     </div>
   );
 }
